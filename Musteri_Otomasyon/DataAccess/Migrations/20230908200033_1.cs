b92cc798f5d06a5d6c3c82917bdc59ddf9b63f5b
MehmetAkinci-44 <mehmet_akinci_44@hotmail.com>
2023-09-13 14:03:15 +0300

    Müşteri otomasyon

---
+++ Musteri_Otomasyon/DataAccess/Migrations/20230908200033_1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Musteris",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "varchar(25)", nullable: false),
+                    Soyadi = table.Column<string>(type: "varchar(25)", nullable: false),
+                    E_mail = table.Column<string>(type: "varchar(75)", nullable: false),
+                    Telefon = table.Column<int>(type: "number(11)", nullable: false),
+                    Doğum_Tarihi = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Meslegi = table.Column<string>(type: "varchar(25)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musteris", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Musteris");
+        }
+    }
+}
---
+++ Musteri_Otomasyon/DataAccess/Migrations/20230912202928_3.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class _3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Soyadi = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Telefon = table.Column<decimal>(type: "decimal(18,2)", maxLength: 11, nullable: false),
+                    E_mail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Kullanici_Adi = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Sifre = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    rol = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+        }
+    }
+}
